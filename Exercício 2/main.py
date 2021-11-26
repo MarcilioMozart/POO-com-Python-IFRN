a72fbf75eda69ed6a1fa6c13d8ebc36fd034f75a
MarcilioMozart <69993737+MarcilioMozart@users.noreply.github.com>
2021-11-26 11:30:02 -0300

    Create main.py
    Arquivo main.py onde a classe Aluno é instanciada

---
+++ Exercício 2/main.py
@@ -0,0 +1,9 @@
+from aluno import Aluno
+
+######### Criando instância da classe Aluno #########
+aluno1 = Aluno("Marcílio Mozart","Informática", 1)
+aluno2 = Aluno("João","Informática", 2)
+
+########## Mostra as informações do Aluno ###########
+print("Nome:",aluno1.nome,"\nCurso:",aluno1.curso,"\nSérie:",aluno1.serie)
+print("Nome:",aluno2.nome,"\nCurso:",aluno2.curso,"\nSérie:",aluno2.serie)
