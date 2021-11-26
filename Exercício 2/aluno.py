a635da70476c2756addf08c8dc8237a35ca55992
MarcilioMozart <69993737+MarcilioMozart@users.noreply.github.com>
2021-11-26 11:30:41 -0300

    Create aluno.py
    Arquivo aluno.py onde é criada a classe Aluno

---
+++ Exercício 2/aluno.py
@@ -0,0 +1,13 @@
+''' Crie uma classe Aluno com as seguintes características: Nome, Curso, Serie'''
+
+class Aluno :
+  nome = ""
+  curso = ""
+  serie = ""
+
+  def __init__(self, pNome, pCurso, pSerie) :
+    self.nome = pNome
+    self.curso = pCurso
+    self.serie = pSerie
+
+################ Fim da Classe Aluno ###############
