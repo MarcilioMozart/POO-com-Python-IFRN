c77729c72ab162d269d6fe2a47d42400e58f6001
MarcilioMozart <69993737+MarcilioMozart@users.noreply.github.com>
2021-11-26 11:14:26 -0300

    main.py
    Chamada da classe Veiculo criando instâncias

---
+++ Exercício 1/main.py
@@ -0,0 +1,8 @@
+from Veiculos import Veiculo
+
+veiculo1 = Veiculo("Chevrolet","Onix", 2020)
+veiculo2 = Veiculo("Volkswagen","Gol", 2017)
+
+print("Veículo 1:", veiculo1.fabricante,veiculo1.modelo, veiculo1.ano)
+
+print("Veículo 2:", veiculo2.fabricante,veiculo2.modelo, veiculo2.ano)
