57b05d0fec706d3119bfb964d57d77bb87e62019
MarcilioMozart <69993737+MarcilioMozart@users.noreply.github.com>
2021-11-26 19:15:38 -0300

    Update Veiculos.py

--- Exercício 1/Veiculos.py
+++ Exercício 1/Veiculos.py
@@ -1,9 +1,9 @@
 '''Criar uma classe de nome veiculo com as seguintes características Fabricante, Modelo e Ano'''
 
 class Veiculo :
-  fabricante = ""
-  modelo = ""
-  ano = ""
+  fabricante = None
+  modelo = None
+  ano = None
 
   def __init__(self,pFabricante,pModelo,pAno):
     self.fabricante = pFabricante
