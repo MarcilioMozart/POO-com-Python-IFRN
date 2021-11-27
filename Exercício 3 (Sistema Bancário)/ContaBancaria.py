d832c26acbf39bca512aa2d655fe95fa330aa249
Marcilio Mozart <69993737+MarcilioMozart@users.noreply.github.com>
2021-11-27 07:19:13 -0300

    Add files via upload

---
+++ Exercício 3 (Sistema Bancário)/ContaBancaria.py
@@ -0,0 +1,68 @@
+class ContaBancaria :
+  numero = None
+  cliente = None
+  saldo = None
+  senha = None
+
+  def __init__(self,pNumero,pCliente,pSaldo,pSenha):
+    self.numero = pNumero
+    self.cliente = pCliente
+    self.__saldo = pSaldo
+    self.__senha = pSenha
+    
+##################### OPÇÕES ########################
+  def depositar(self,valor):
+    if valor > 0 :
+      self.__saldo += valor
+    else :
+      print("Valor digitado é inválido! ")
+       
+  def sacar(self,valor):
+    if self.__saldo > 0 and self.__saldo >= valor :
+      self.__saldo -= valor
+    else :
+      print("\n--------------------")
+      print("Saldo insuficiente! ")
+
+  def imprimirExtrato(self):
+    print("O seu Saldo é: R$",self.__saldo)
+
+##################### Login #########################
+  def login(self,lNumero,lSenha):
+    if lNumero == self.numero and lSenha == self.__senha :
+      return True
+      print("Logado")
+    else :
+      return False
+
+ ################# Autentica Usuário ################
+  def autenticarUsuario(self):
+    usuarioLogado = None
+
+    while(usuarioLogado != True):
+      numeroConta = int(input("Digite o número da sua conta: "))
+      senha = input("Digite sua senha: ")
+      print("--------------------")
+      
+      usuarioLogado = self.login(numeroConta,senha)
+  
+      if usuarioLogado == True :
+        print("Usuário Autenticado!")
+      else :
+        print("Número de conta ou senha incorreto!\n")
+#####################################################
+  def escolherOpcao(self):
+    self.autenticarUsuario()
+    op = None
+    while op != 'S' :
+      print("--------------------")
+      op = input("\nDigite uma opção:\n1-Sacar\n2-Depositar\n3-Consultar saldo\nS-Sair\n")
+      print("--------------------")
+      if op == '1' :
+        valor = int(input("Digite um valor para sacar: "))
+        self.sacar(valor)
+      elif op == '2' :
+        valor = int(input("Digite um valor para Depositar:")) 
+        self.depositar(valor)
+      elif op == '3' :
+       self.imprimirExtrato()
---
+++ Exercício 3 (Sistema Bancário)/main.py
@@ -0,0 +1,7 @@
+from ContaBancaria import ContaBancaria
+
+cliente1 = ContaBancaria(12345,"Usuário1",1000,"12345")
+
+################### Escolhe Opção ###################
+
+cliente1.escolherOpcao()
